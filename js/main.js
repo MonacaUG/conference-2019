85385cdbb19c9ab1f9bff651f4e06d74dac2164c
Atsushi <atsushi@moongift.jp>
2019-07-18 14:32:13 +0900

    Set up form

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ $(function(){
     spSubNav();
     
 });//end jquery
+
+if (typeof MauticSDKLoaded == 'undefined') {
+    var MauticSDKLoaded = true;
+    var head            = document.getElementsByTagName('head')[0];
+    var script          = document.createElement('script');
+    script.type         = 'text/javascript';
+    script.src          = 'https://ma.moongift.co.jp/media/js/mautic-form.js';
+    script.onload       = function() {
+        MauticSDK.onLoad();
+    };
+    head.appendChild(script);
+    var MauticDomain = 'https://ma.moongift.co.jp';
+    var MauticLang   = {
+        'submittingMessage': "お待ちください…"
+    }
+}
