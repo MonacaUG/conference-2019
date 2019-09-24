97a0608f68fbd178707921d097d1bd575fd8acdc
Atsushi <atsushi@moongift.jp>
2019-09-24 14:21:20 +0900

    Add TAM

--- getData.sh
+++ getData.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -L "https://script.google.com/macros/s/AKfycbwUMiy2e11KFZ8ZcbCh8uU_GiPgWs-1mqZEvzr5_9VKVpoELWv3/exec?name=organizers" -o _data/organizers.json
 curl -L "https://script.google.com/macros/s/AKfycbwUMiy2e11KFZ8ZcbCh8uU_GiPgWs-1mqZEvzr5_9VKVpoELWv3/exec?name=sessions" -o _data/sessions.json
+curl -L "https://script.google.com/macros/s/AKfycbwUMiy2e11KFZ8ZcbCh8uU_GiPgWs-1mqZEvzr5_9VKVpoELWv3/exec?name=sponsors" -o _data/sponsors.json
 curl -L "https://script.google.com/macros/s/AKfycbwUMiy2e11KFZ8ZcbCh8uU_GiPgWs-1mqZEvzr5_9VKVpoELWv3/exec?name=japan-ele" -o _data/japanele.json
 
 ruby speaker.rb
