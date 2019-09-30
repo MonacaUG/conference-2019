0fc9e582a101dea57743b005ec88134c3d9eb517
Atsushi <atsushi@moongift.jp>
2019-09-30 11:41:39 +0900

    Add speaker

--- markdown.rb
+++ markdown.rb
@@ -1,20 +1,27 @@
 require('json')
 json = JSON.parse(open('./_data/sessions.json').read)
+eles = JSON.parse(open('./_data/japanele.json').read)
 lines = [['時間', 'トラック', 'タイトル', '名前']]
 lines << ['---', '---', '---', '---']
 json.each do |session|
   next if session['hidden'] == 'true'
-  if session['id'] != ''
-    speaker = session['name']
-    if session['twitter'] != ""
-      speaker = "[#{speaker}](https://twitter.com/#{session['twitter']})"
-    end
-    speaker += session['company'] == '' ? '' : "@#{session['company']}"
+  if session['group'] == 'true'
+    track = session['track']
+    title = eles.map{|ele| "[#{ele['title']}](https://monacaug.mobi/conference-2019/speakers/#{ele['id']}/)"}.join('<br />')
+    speaker = eles.map{|ele| "#{ele['name']}@日本電子専門学校"}.join('<br />')
   else
-    speaker = '後日アップデート' if session['speaker'] == 'false'
+    if session['id'] != ''
+      speaker = session['name']
+      if session['twitter'] != ""
+        speaker = "[#{speaker}](https://twitter.com/#{session['twitter']})"
+      end
+      speaker += session['company'] == '' ? '' : "@#{session['company']}"
+    else
+      speaker = '後日アップデート' if session['speaker'] == 'false'
+    end
+    track = session['track'] == '3' ? '' : session['track']
+    title = ['false', ''].include?(session['speaker'])  ? session['title'] : "[#{session['title']}](https://monacaug.mobi/conference-2019/speakers/#{session['id']}/)"
   end
-  track = session['track'] == '3' ? '' : session['track']
-  title = ['false', ''].include?(session['speaker'])  ? session['title'] : "[#{session['title']}](https://monacaug.mobi/conference-2019/speakers/#{session['id']}/)"
   lines << [session['time'], track, title, speaker]
 end
 
